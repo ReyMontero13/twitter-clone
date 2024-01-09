4afd5ff06258e9d3e47833b10bba517b1a3dcfef
ReyMonter13 <rlm@dealogikal.com>
2024-01-10 01:01:50 +0800

    okay

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -40,7 +40,7 @@ const Avatar: React.FC<AvatarProps> = ({
                 relative
             `}
         >
-            <Image fill style={{objectFit: 'cover'}} alt="Avatar" onClick={onClick} src={fetchedUser?.profileImage || '/images/placeholder.png'}/>
+            <Image fill style={{objectFit: 'cover', borderRadius:'100%'}} alt="Avatar" onClick={onClick} src={fetchedUser?.profileImage || '/images/placeholder.jpg'}/>
 
         </div>
     );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from 'next-auth/react';
 import Layout from '@/components/Layout'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
+import EditModal from '@/components/modals/EditModal'
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal/>
       <RegisterModal />
       <LoginModal />
       <Layout>
